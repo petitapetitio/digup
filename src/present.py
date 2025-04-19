55732c4b5245efef35328a8a65ad9ee3dca26502
lxnd <lxnd@protonmail.com>
2025-04-19 09:11:55 +0200

    refactor table presenter (wip)

--- src/present.py
+++ src/present.py
@@ -8,7 +8,34 @@ from src.count_words import WordCount
 from src.get_nodes import Node
 
 
+@dataclass(frozen=True)
+class Table:
+    columns: dict[str, _Column]
+    table_width: int
+
+    @classmethod
+    def of(cls, columns: list[_Column]) -> Table:
+        index = {}
+        for c in columns:
+            index[c.name] = c
+        table_width = sum(c.width for c in index.values())
+        return Table(index, table_width)
+
+    def header(self) -> str:
+        return "".join(c.present_header() for c in self.columns.values()) + "\n"
+
+    def separator(self) -> str:
+        return "-" * self.table_width + "\n"
+
+
 def present_word_count(word_count: WordCount) -> str:
+    table = Table.of([
+        _Column("word", 40, "<"),
+        _Column("#", 10, ">"),
+        _Column("span", 10, ">"),
+        _Column("proportion", 12, ">"),
+    ])
+
     columns = {
         "word": _Column("word", 40, "<"),
         "occurences": _Column("#", 10, ">"),
@@ -16,13 +43,10 @@ def present_word_count(word_count: WordCount) -> str:
         "proportion": _Column("proportion", 12, ">"),
     }
 
-    header = "".join(c.present_header() for c in columns.values())
-    array_width = sum(c.width for c in columns.values())
-
     res = ""
-    res += "-" * array_width + "\n"
-    res += header + "\n"
-    res += "-" * array_width + "\n"
+    res += table.separator()
+    res += table.header()
+    res += table.separator()
 
     for word in word_count.words:
         line = (
@@ -85,18 +109,18 @@ def present_nodes(nodes: list[LsItem], kind: str):
 
 @dataclass(frozen=True)
 class _Column:
-    header: str
+    name: str
     width: int
-    indent: str
+    align: str
 
     def present_header(self) -> str:
-        return f"{self.header: {self.indent}{self.width}}"
+        return f"{self.name: {self.align}{self.width}}"
 
     def str_value(self, value: str) -> str:
-        return f"{value: {self.indent}{self.width}}"
+        return f"{value: {self.align}{self.width}}"
 
     def int_value(self, value: int) -> str:
-        return f"{value: {self.indent}{self.width}}"
+        return f"{value: {self.align}{self.width}}"
 
     def percentage(self, normalized_percentage: float) -> str:
-        return f"{normalized_percentage: {self.indent}{self.width}.0%}"
+        return f"{normalized_percentage: {self.align}{self.width}.0%}"
