956b205001a9d655fb68eaac876ea57234dba508
lxnd <lxnd@protonmail.com>
2025-04-20 09:02:29 +0200

    refactor: cleanup color code

--- src/highlight_identifiers.py
+++ src/highlight_identifiers.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 from textwrap import dedent
 from typing import Optional, TypeAlias
 
-from src.color import hue_gradient, colorize
-from src.count_words import get_identifiers, IdentifierKind
+from src.colorize import hue_gradient, colorize
+from src.count_words import get_identifiers, IdentifierKind, Identifier
 
 RGBColor: TypeAlias = tuple[int, int, int]
 
@@ -33,21 +33,24 @@ def highlight_identifiers(
         if identifier.name not in color_by_identifier:
             color_by_identifier[identifier.name] = next(colors_iterator)
         identifiers_by_line[identifier.lineno].append(identifier)
-    highlighted_lines = []
 
+    return present_highlights(color_by_identifier, identifiers_by_line, source)
+
+
+def present_highlights(color_by_identifier, identifiers_by_line: dict[int, list[Identifier]], source: str):
+    output_lines = []
     for lineno, line in enumerate(source.splitlines(), start=1):
         if lineno not in identifiers_by_line:
-            highlighted_lines.append(line)
+            output_lines.append(line)
             continue
 
-        highlighted_line = line
+        output_line = line
         for identifier in reversed(identifiers_by_line[lineno]):
             highlighted_identifier = colorize(identifier.name, foreground=color_by_identifier[identifier.name])
-            highlighted_line = (
-                highlighted_line[: identifier.column]
+            output_line = (
+                output_line[: identifier.column]
                 + highlighted_identifier
-                + highlighted_line[identifier.column + len(identifier.name) :]
+                + output_line[identifier.column + len(identifier.name) :]
             )
-        highlighted_lines.append(highlighted_line)
-
-    return "\n".join(highlighted_lines) + "\n"
+        output_lines.append(output_line)
+    return "\n".join(output_lines) + "\n"
