d6fecce787538a9608e172cbcb5f3c9a63647af3
lxnd <lxnd@protonmail.com>
2025-04-20 09:56:03 +0200

    refactor: decouple highlight feature

--- src/highlight_identifiers.py
+++ src/highlight_identifiers.py
@@ -1,43 +1,66 @@
 import ast
 from collections import defaultdict
+from dataclasses import dataclass
 from textwrap import dedent
-from typing import Optional, TypeAlias
+from typing import Optional
 
 from src.colorize import hue_gradient, colorize
-from src.count_words import get_identifiers, IdentifierKind, Identifier
+from src.count_words import get_identifiers, IdentifierKind
 
-RGBColor: TypeAlias = tuple[int, int, int]
+
+@dataclass(frozen=True)
+class IdentifierToHighlight:
+    name: str
+    column: int
+    color: int
 
 
 def highlight_identifiers(
     code: str,
+    *,
+    colors: Optional[list[int]] = None,
     only: Optional[set[str]] = None,
     params_only: bool = False,
-) -> str:
+) -> dict[int, list[IdentifierToHighlight]]:
     # TODO : je parse le code plusieurs fois ici, c'est un peu bête
     source = dedent(code)
     tree = ast.parse(source)
 
-    color_by_identifier = {}
-    identifiers_by_line: dict[int, list] = defaultdict(list)
     sorted_identifiers = sorted(get_identifiers(tree), key=lambda idtf: (idtf.lineno, idtf.column))
 
-    colors_iterator = hue_gradient(len({i.name for i in sorted_identifiers}))
+    if params_only:
+        for identifier in sorted_identifiers:
+            if identifier.kind == IdentifierKind.ARG:
+                if only is None:
+                    only = set()
+                only.add(identifier.name)
 
-    for identifier in sorted_identifiers:
-        if only is not None and identifier.name not in only:
-            continue
-        if params_only and identifier.kind != IdentifierKind.ARG and identifier.name not in color_by_identifier:
-            continue
+    if only is None:
+        filtered_identifiers = sorted_identifiers
+    else:
+        filtered_identifiers = []
+        for identifier in sorted_identifiers:
+            if identifier.name in only:
+                filtered_identifiers.append(identifier)
 
-        if identifier.name not in color_by_identifier:
-            color_by_identifier[identifier.name] = next(colors_iterator)
-        identifiers_by_line[identifier.lineno].append(identifier)
+    names = {i.name: i for i in filtered_identifiers}
 
-    return present_highlights(color_by_identifier, identifiers_by_line, source)
+    if colors is None:
+        colors = hue_gradient(len(names))
+
+    colors_by_name = {name: color for name, color in zip(names.keys(), colors)}
+
+    identifiers_by_line: dict[int, list[IdentifierToHighlight]] = defaultdict(list)
+    for identifier in filtered_identifiers:
+        identifiers_by_line[identifier.lineno].append(
+            IdentifierToHighlight(identifier.name, identifier.column, colors_by_name[identifier.name])
+        )
+
+    return dict(identifiers_by_line)
 
 
-def present_highlights(color_by_identifier, identifiers_by_line: dict[int, list[Identifier]], source: str):
+def present_highlights(identifiers_by_line: dict[int, list[IdentifierToHighlight]], source: str) -> str:
+    source = dedent(source)
     output_lines = []
     for lineno, line in enumerate(source.splitlines(), start=1):
         if lineno not in identifiers_by_line:
@@ -46,7 +69,7 @@ def present_highlights(color_by_identifier, identifiers_by_line: dict[int, list[
 
         output_line = line
         for identifier in reversed(identifiers_by_line[lineno]):
-            highlighted_identifier = colorize(identifier.name, foreground=color_by_identifier[identifier.name])
+            highlighted_identifier = colorize(identifier.name, foreground=identifier.color)
             output_line = (
                 output_line[: identifier.column]
                 + highlighted_identifier
